03d045f674260389835532e28d1edc094a1ccadb
advrxh <advrxh@gmail.com>
2024-08-05 21:37:57 +0530

    Remove unused configuration methods

--- pilip-backend/app.py
+++ pilip-backend/app.py
@@ -1,11 +1,9 @@
 from contextlib import asynccontextmanager
 
 from aioredis import Redis, RedisError, from_url
-from dotenv import load_dotenv
 from fastapi import FastAPI, Request, Depends
 from tortoise.contrib.fastapi import register_tortoise
 
-load_dotenv()
 
 from pilip.constants import Config
 from pilip.routers import events_router, submissions_router, auth_router
@@ -14,9 +12,7 @@ from pilip.dependencies.redis import get_redis, redis_error_handler
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    app.state.redis = await from_url(
-        url="redis://localhost", username="root", password="root"
-    )
+    app.state.redis = await from_url(url=Config.REDIS_URL)
 
     await app.state.redis.rpush("tokens", "0")
     await app.state.redis.expire("tokens", 24 * 60 * 60)
--- pilip-backend/pilip/constants.py
+++ pilip-backend/pilip/constants.py
@@ -1,27 +1,13 @@
 import os
 from datetime import datetime
 
-from pydantic import BaseModel
-
-
-def _redis_configs(self):
-
-    configs = {}
-
-    configs["host"] = os.getenv("REDIS_HOST", "localhost")
-    configs["port"] = os.getenv("REDIS_PORT", 6379)
-    configs["username"] = os.getenv("REDIS_USER", "root")
-    configs["password"] = os.getenv("REDIS_PASS", "root")
-
-    return configs
-
 
 class Config:
 
     DEBUG = os.getenv("DEBUG", "false") == "true"
     STARTUP = datetime.now()
     DB_URL = os.getenv("DB_URL", "postgres://root:root@localhost:5435/pilip")
-    REDIS_URL = os.getenv("REDIS_URL", "redis://root:root@localhost:6379/")
+    REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/")
 
     AUTH_LIST = os.getenv("AUTH", "root:root,username:password")
 
